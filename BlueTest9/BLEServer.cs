af3c6a8a6e61e1f7228097fae1c52a064dbadcdf
Nicholas Phillips <nwp2@illinois.edu>
2025-02-06 20:19:23 -0600

    first

---
+++ BlueTest9/BLEServer.cs
@@ -0,0 +1,191 @@
+using System;
+using Windows.Foundation;
+using Windows.Devices.Bluetooth;
+using System.Threading.Tasks;
+using Windows.Devices.Bluetooth.GenericAttributeProfile;
+using Windows.Storage.Streams;
+
+namespace BlueTest9
+{
+    public enum BallValveAction
+    {
+        Open,
+        Close,
+        AutoArm,
+        AutoDisarm,
+        None
+    }
+    public class BLEServer
+    {
+        private static readonly Guid serviceID = Guid.Parse("47da423a-5990-4b7a-b193-9b56c95926b1");
+        private static readonly Guid daqID = Guid.Parse("55c39af6-6f39-4ba6-8d49-6066c30ca1e9");
+        private static readonly Guid actuateID = Guid.Parse("07ecb91a-5534-44ca-8a97-942ef72e4e6b");
+        private static readonly int OpenCode = 129035867;
+        private static readonly int CloseCode = 2065891205;
+        private static readonly int ArmCode = 671238032;
+        private static readonly int DisarmCode = 301529212;
+        private GattServiceProvider serviceProvider;
+        private GattLocalCharacteristic DAQCharacteristic;
+        private GattLocalCharacteristic actuateCharacteristic;
+        public float force = 0;
+        public float pressure1 = 0;
+        public float pressure2 = 0;
+        public float det_voltage = 0;
+        public bool ballValveOpen = false;
+        public bool is_auto_armed = false;
+        public BallValveAction action = BallValveAction.None;
+        private static readonly GattLocalCharacteristicParameters ReadParams = new GattLocalCharacteristicParameters
+        {
+            CharacteristicProperties = GattCharacteristicProperties.Read | GattCharacteristicProperties.Notify,
+            WriteProtectionLevel = GattProtectionLevel.Plain,
+            UserDescription = "DAQ Characteristc",
+        };
+
+        private static readonly GattLocalCharacteristicParameters ActuateParams = new GattLocalCharacteristicParameters
+        {
+            CharacteristicProperties = GattCharacteristicProperties.Write | GattCharacteristicProperties.WriteWithoutResponse,
+            WriteProtectionLevel = GattProtectionLevel.Plain,
+            UserDescription = "Actuate Characteristc",
+        };
+        private static async Task<bool> IsPeripheralModeSupported()
+        {
+            // BT_Code: New for Creator's Update - Bluetooth adapter has properties of the local BT radio.
+            var localAdapter = await BluetoothAdapter.GetDefaultAsync();
+
+            if (localAdapter != null)
+            {
+                return localAdapter.IsPeripheralRoleSupported;
+            }
+            else
+            {
+                // Bluetooth is not turned on 
+                return false;
+            }
+        }
+        private async Task<GattLocalCharacteristic> MakeCharacteristic(Guid guid, GattLocalCharacteristicParameters parameters)
+        {
+            GattLocalCharacteristicResult result = await serviceProvider.Service.CreateCharacteristicAsync(guid, parameters);
+            if (result.Error != BluetoothError.Success)
+            {
+                Console.WriteLine("Error creating characteristic");
+                return null;
+            }
+            return result.Characteristic;
+        }
+        public async Task<bool> start()
+        {
+            if (!await IsPeripheralModeSupported())
+            {
+                Console.WriteLine("Error, bluetooth peripheral mode not supported");
+                return false;
+            }
+            GattServiceProviderResult serviceResult = await GattServiceProvider.CreateAsync(serviceID);
+            if (serviceResult.Error != BluetoothError.Success)
+            {
+                Console.WriteLine("Error creating service provider");
+                return false;
+            }
+
+            serviceProvider = serviceResult.ServiceProvider;
+
+            DAQCharacteristic = await MakeCharacteristic(daqID, ReadParams);
+            actuateCharacteristic = await MakeCharacteristic(actuateID, ActuateParams);
+
+            DAQCharacteristic.ReadRequested += DAQReadAsync;
+            actuateCharacteristic.WriteRequested += ActuateWriteAsync;
+
+            GattServiceProviderAdvertisingParameters advParams = new GattServiceProviderAdvertisingParameters
+            {
+                IsConnectable = true,
+                IsDiscoverable = true,
+            };
+
+            serviceProvider.AdvertisementStatusChanged += AdvertismentStatusChanged;
+            serviceProvider.StartAdvertising(advParams);
+            return true;
+        }
+
+
+        private void AdvertismentStatusChanged(GattServiceProvider sender, GattServiceProviderAdvertisementStatusChangedEventArgs args)
+        {
+            Console.WriteLine($"New Advertising Status: {sender.AdvertisementStatus}");
+        }
+
+        private async void DAQReadAsync(GattLocalCharacteristic sender, GattReadRequestedEventArgs args)
+        {
+            using (args.GetDeferral())
+            {
+                GattReadRequest request = await args.GetRequestAsync();
+                if (request == null)
+                {
+                    Console.WriteLine("Access to device not allowed (does the app have bluetooth permissions?)");
+                    return;
+                }
+
+                var writer = new DataWriter();
+                writer.ByteOrder = ByteOrder.LittleEndian;
+                writer.WriteSingle(force);
+                writer.WriteSingle(pressure1);
+                writer.WriteSingle(pressure2);
+                writer.WriteSingle(det_voltage);
+                writer.WriteBoolean(ballValveOpen);
+                writer.WriteBoolean(is_auto_armed);
+                request.RespondWithValue(writer.DetachBuffer());
+            }
+        }
+        private async void ActuateWriteAsync(GattLocalCharacteristic sender, GattWriteRequestedEventArgs args)
+        {
+            using (args.GetDeferral())
+            {
+                // Get the request information.  This requires device access before an app can access the device's request.
+                GattWriteRequest request = await args.GetRequestAsync();
+                if (request == null)
+                {
+                    // No access allowed to the device.  Application should indicate this to the user.
+                    return;
+                }
+
+                if (request.Value.Length != 4)
+                {
+                    if (request.Option == GattWriteOption.WriteWithResponse)
+                    {
+                        request.RespondWithProtocolError(GattProtocolError.InvalidAttributeValueLength);
+                    }
+                    return;
+                }
+
+                var reader = DataReader.FromBuffer(request.Value);
+                reader.ByteOrder = ByteOrder.LittleEndian;
+                int val = reader.ReadInt32();
+
+                if (val == OpenCode)
+                {
+                    Console.WriteLine("OPEN");
+                    action = BallValveAction.Open;
+                }
+                else if (val == CloseCode)
+                {
+                    Console.WriteLine("CLOSE");
+                    action = BallValveAction.Close;
+                }
+                else if (val == ArmCode)
+                {
+                    Console.WriteLine("ARM");
+                    action = BallValveAction.AutoArm;
+                } 
+                else if (val == DisarmCode)
+                {
+                    Console.WriteLine("DISARM");
+                    action = BallValveAction.AutoDisarm;
+                } 
+                else {
+                    Console.WriteLine($"Unrecognized val {val}");
+                }
+                if (request.Option == GattWriteOption.WriteWithResponse)
+                {
+                    request.Respond();
+                }
+            }
+        }
+    }
+}
---
+++ BlueTest9/Program.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.IO.Ports;
+using Windows.Storage;
+using System.IO;
+using Windows.System;
+using Windows.UI.Xaml.Automation.Peers;
+using System.Linq.Expressions;
+using Windows.Gaming.Input.ForceFeedback;
+
+// This example code shows how you could implement the required main function for a 
+// Console UWP Application. You can replace all the code inside Main with your own custom code.
+
+// You should also change the Alias value in the AppExecutionAlias Extension in the 
+// Package.appxmanifest to a value that you define. To edit this file manually, right-click
+// it in Solution Explorer and select View Code, or open it with the XML Editor.
+
+namespace BlueTest9
+{
+    class Program {
+        public static BLEServer server;
+        public static bool should_quit;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter COM port");
+            string port_address = Console.ReadLine();
+            server = new BLEServer();
+            var started = server.start();
+            Task.WaitAll(started);
+            if (!started.Result)
+            {
+                Console.WriteLine("Server Setup Failed");
+                return;
+            }
+            Console.WriteLine("Bluetooth Server Open");
+            Task serialTask = Task.Run(()=>RunSerial(port_address));
+
+
+            while (!should_quit)
+            {
+                string line = Console.ReadLine();
+                switch (line)
+                {
+                    case "a":
+                        server.action = BallValveAction.AutoArm;
+                        break;
+                    case "d":
+                        server.action = BallValveAction.AutoDisarm;
+                        break;
+                    case "o":
+                        server.action = BallValveAction.Open;
+                        break;
+                    case "c":
+                        server.action = BallValveAction.Close;
+                        break;
+                    case "quit":
+                        should_quit = true;
+                        break;
+                    default:
+                        Console.WriteLine("unrecognized action {0}", line);
+                        break;
+                }
+            }
+            Task.WaitAll(serialTask);
+        }
+        static async Task RunSerial(string address)
+        {
+            Console.WriteLine("Running Serial");
+            try
+            {
+                SerialPort port = new SerialPort(address, 38400);
+                StorageFile file = await DownloadsFolder.CreateFileAsync(DateTime.Now.ToString("yyyy.MM.dd.HH.mm.ss") + ".csv");
+                Console.WriteLine($"Logging to {file.Path}");
+                StreamWriter csvLog = new StreamWriter(await file.OpenStreamForWriteAsync());
+                port.Open();
+                Console.WriteLine("Port opened");
+                csvLog.WriteLine("millis,force,p1,p2,p3,dev_volt,is_open,is_auto_armed,ematch_detect_time");
+                port.Write(" ");
+                double avg1 = 0.0;
+                double avg2 = 0.0;
+                double avg3 = 0.0;
+                
+                while (!should_quit)
+                {
+                    if (server.action == BallValveAction.Close)
+                    {
+                        port.Write("c");
+                        server.action = BallValveAction.None;
+                    }
+                    if (server.action == BallValveAction.Open)
+                    {
+                        port.Write("o");
+                        server.action = BallValveAction.None;
+                    }
+                    if (server.action == BallValveAction.AutoArm)
+                    {
+                        port.Write("a");
+                        server.action = BallValveAction.None;
+                    }
+                    if (server.action == BallValveAction.AutoDisarm)
+                    {
+                        port.Write("d");
+                        server.action = BallValveAction.None;
+                    }
+
+                    string line = port.ReadLine();
+                    string[] split = line.Split(new char[] { '\t', '\r', ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                    if (split.Length != 9)
+                    {
+                        continue;
+                    }
+                    try
+                    {
+                        int millis = int.Parse(split[0]);
+                        float force = float.Parse(split[1]);
+                        float pressure1 = float.Parse(split[2]);
+                        float pressure2 = float.Parse(split[3]);
+                        float pressure3 = float.Parse(split[4]);
+                        float det_voltage = float.Parse(split[5]);
+                        int is_open = int.Parse(split[6]);
+                        int is_auto_armed = int.Parse(split[7]);
+                        int ematch_detected_time = int.Parse(split[7]);
+                        avg1 = avg1 * 0.9 + (float)pressure1 * 0.1;
+                        avg2 = avg2 * 0.9 + (float)pressure2 * 0.1;
+                        avg3 = avg3 * 0.9 + (float)pressure3 * 0.1;
+                        server.force = 100;
+                        server.force = force;
+                        server.pressure1 = (float)avg1;
+                        server.pressure2 = (float)avg2;
+                        server.ballValveOpen = (is_open == 1);
+                        server.det_voltage = det_voltage;
+                        server.is_auto_armed = (is_auto_armed == 1);
+                        csvLog.WriteLine("{0},{1},{2},{3},{4},{5},{6},{7},{8}", millis, force, pressure1, pressure2, pressure3, det_voltage, is_open, is_auto_armed, ematch_detected_time);
+                        Console.Write("\r{0},{1},{2},{3},{4},{5},{6},{7}...............", millis, force, pressure1, pressure2, pressure3, det_voltage, is_open, is_auto_armed);
+                    }
+                    catch (System.FormatException e)
+                    {
+                        Console.WriteLine(e.ToString());
+                    }
+
+                }
+
+                port.Close();
+                csvLog.Close();
+            } catch(System.AggregateException e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
